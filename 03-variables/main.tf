18ab215172eccc31a6d9e837c2dc3cfe8d6118f7
rajendra <rajendrammarboyana@gmail.com>
2024-04-03 09:56:48 +0100

    variables

--- 03-variables/main.tf
+++ 03-variables/main.tf
@@ -1,7 +1,20 @@
 variable "x" {
   default = 54
 }
+
+variable "y_list" {
+  default = [1,7,8]
+}
+
+
+
+
+
 #print the output
 output "x" {
   value = var.x
+}
+#print the output
+output "y" {
+  value = var.y_list
 }
