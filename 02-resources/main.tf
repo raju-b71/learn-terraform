a71b89956b5ce925e2f5c0603235e35db7106b08
rajendra <rajendrammarboyana@gmail.com>
2024-04-03 09:27:38 +0100

    creating instances

--- 02-resources/main.tf
+++ 02-resources/main.tf
@@ -1,4 +1,24 @@
 resource "aws_instance" "test" {
   ami           = "ami-05f020f5935e52dc4"
-  instance_type = "t3.small"
+  instance_type = "t3.micro"
+  tags = {
+    name = "frontend"
+  }
 }
+
+resource "aws_instance" "test" {
+  ami           = "ami-05f020f5935e52dc4"
+  instance_type = "t3.micro"
+  tags = {
+    name = "backend"
+  }
+}
+
+resource "aws_instance" "test" {
+  ami           = "ami-05f020f5935e52dc4"
+  instance_type = "t3.micro"
+  tags = {
+    name = "mysql"
+  }
+}
+
