abea97d4f51abb3b0771aa5553adbc190bb2b4c5
rajendra <rajendrammarboyana@gmail.com>
2024-04-03 13:37:01 +0100

    modules

--- 02-resources/main.tf
+++ 02-resources/main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "frontend" {
+ resource "aws_instance" "frontend" {
   ami           = var.ami
   instance_type = var.instance_type
   vpc_security_group_ids = var.security_groups
