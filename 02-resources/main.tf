87e1a0cfe3c9ecb4c704e0644186b36b2f0aed8b
rajendra <rajendrammarboyana@gmail.com>
2024-04-03 09:35:22 +0100

    creating instances for expense

--- 02-resources/main.tf
+++ 02-resources/main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "test" {
+resource "aws_instance" "frontend" {
   ami           = "ami-05f020f5935e52dc4"
   instance_type = "t3.micro"
   vpc_security_group_ids = ["sg-0ba469f077949363e"]
@@ -7,7 +7,7 @@ resource "aws_instance" "test" {
   }
 }
 
-resource "aws_instance" "test" {
+resource "aws_instance" "backend" {
   ami           = "ami-05f020f5935e52dc4"
   instance_type = "t3.micro"
   vpc_security_group_ids = ["sg-0ba469f077949363e"]
@@ -16,7 +16,7 @@ resource "aws_instance" "test" {
   }
 }
 
-resource "aws_instance" "test" {
+resource "aws_instance" "mysql" {
   ami           = "ami-05f020f5935e52dc4"
   instance_type = "t3.micro"
   vpc_security_group_ids = ["sg-0ba469f077949363e"]
